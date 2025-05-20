009c9aa10807aff1a64d014014e0ed69ee8cab28
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-05-19 23:03:07 -0400

    prettier format 1

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,32 +1,31 @@
 <template>
   <q-page class="flex flex-center">
     <div class="w-full flex flex-col items-center">
-      <q-btn @click="handleLogout">Logout</q-btn>
       <Sucursales class="mt-8" />
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { definePageMeta } from '#imports';
-import Sucursales from '~/components/Sucursales.vue';
+import { defineComponent } from "vue";
+import { definePageMeta } from "#imports";
+import Sucursales from "~/components/Sucursales.vue";
 
 definePageMeta({
-  middleware: 'auth' // o ['auth'] si tienes varios
+  middleware: "auth", // o ['auth'] si tienes varios
 });
 
 export default defineComponent({
-  name: 'DashboardPage',
+  name: "DashboardPage",
   components: {
-    Sucursales
+    Sucursales,
   },
   methods: {
     handleLogout() {
       // aquí va tu lógica de logout
-      console.log('Cerrar sesión');
-    }
-  }
+      console.log("Cerrar sesión");
+    },
+  },
 });
 </script>
 
